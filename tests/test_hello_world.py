e0ba3bf3090dbdb4662fcacc77049afa14bd70dc
Meret Behrens <meret.behrens@tngtech.com>
2023-03-09 14:52:38 +0100

    [issue_4] add isort, black, flake8 and mypy
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/test_hello_world.py
+++ tests/test_hello_world.py
@@ -1,9 +1,9 @@
-# SPDX-FileCopyrightText: Meret Behrens
 # SPDX-FileCopyrightText: TNG Technology Consulting GmbH <https://www.tngtech.com>
-
 # SPDX-License-Identifier: Apache-2.0
+import pytest
 
-def test_print_hello_world(capsys):
+
+def test_print_hello_world(capsys: pytest.CaptureFixture) -> None:
     print("Hello World")
 
     captured = capsys.readouterr()
