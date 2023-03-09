6ca016baa11606604ff36388c6f7cdf2ef5030f3
Meret Behrens <meret.behrens@tngtech.com>
2023-03-09 14:52:38 +0100

    [issue_4] add isort, black, flake8 and mypy
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ scripts/linter_and_formatting.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"/..
+
+echo "sorting imports"
+echo "-----------------------"
+poetry run isort src/ tests/
+
+echo "autoformatting code"
+echo "-----------------------"
+poetry run black src/ tests/
+
+echo "run flake"
+poetry run flake8 src/ tests/
+
+echo "running mypy"
+echo "-----------------------"
+
+poetry run python -m mypy src/ tests/
