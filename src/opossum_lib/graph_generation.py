66e114816406f759b378fc31c6a2a90929876101
Meret Behrens <meret.behrens@tngtech.com>
2023-03-15 15:47:11 +0100

    [issue_11] add graph generation for SPDX document
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/opossum_lib/graph_generation.py
+++ src/opossum_lib/graph_generation.py
@@ -1,8 +1,11 @@
 # SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
 #
 # SPDX-License-Identifier: Apache-2.0
+from typing import Dict, List
+
 from networkx import DiGraph  # type: ignore
 from spdx.model.document import Document
+from spdx.model.relationship import Relationship
 
 
 def generate_graph_from_spdx_lite(document: Document) -> DiGraph:
@@ -19,3 +22,45 @@ def generate_graph_from_spdx_lite(document: Document) -> DiGraph:
     graph.add_edges_from(("DESCRIBES", spdx_id) for spdx_id in package_spdx_ids)
 
     return graph
+
+
+def generate_graph_from_file(document: Document) -> DiGraph:
+    graph = DiGraph()
+    graph.add_node(document.creation_info.spdx_id, element=document.creation_info)
+
+    relationships_by_spdx_id: Dict[
+        str, List[Relationship]
+    ] = get_relationships_by_spdx_id(document.relationships)
+
+    for spdx_id in relationships_by_spdx_id.keys():
+        if spdx_id not in graph.nodes():
+            graph.add_node(spdx_id)
+        for relationship in relationships_by_spdx_id[spdx_id]:
+            relationship_node_key = (
+                str(relationship.spdx_element_id)
+                + "_"
+                + relationship.relationship_type.name
+            )
+            graph.add_node(relationship_node_key, comment=relationship.comment)
+            graph.add_edge(relationship.spdx_element_id, relationship_node_key)
+            # if the related spdx element is SpdxNone or SpdxNoAssertion we need a
+            # type conversion
+            spdx_element_id = str(relationship.related_spdx_element_id)
+
+            if spdx_element_id not in graph.nodes():
+                graph.add_node(spdx_element_id)
+            graph.add_edge(relationship_node_key, spdx_element_id)
+
+    return graph
+
+
+def get_relationships_by_spdx_id(
+    relationships: List[Relationship],
+) -> Dict[str, List[Relationship]]:
+    relationships_by_spdx_id: Dict[str, List[Relationship]] = dict()
+    for relationship in relationships:
+        relationships_by_spdx_id.setdefault(relationship.spdx_element_id, []).append(
+            relationship
+        )
+
+    return relationships_by_spdx_id
--- tests/test_graph_generation.py
+++ tests/test_graph_generation.py
@@ -3,9 +3,13 @@
 # SPDX-License-Identifier: Apache-2.0
 import os
 
+import pytest
 from spdx.parser.parse_anything import parse_file
 
-from opossum_lib.generate_graph_from_spdx_lite import generate_graph_from_spdx_lite
+from opossum_lib.graph_generation import (
+    generate_graph_from_file,
+    generate_graph_from_spdx_lite,
+)
 
 
 def test_generate_graph_from_spdx_lite() -> None:
@@ -18,3 +22,19 @@ def test_generate_graph_from_spdx_lite() -> None:
     assert document.creation_info.spdx_id in graph.nodes()
     assert graph.number_of_nodes() == 4
     assert len(graph.edges()) == 3
+
+
+@pytest.mark.parametrize(
+    "file_path, nodes_count, edges_count",
+    [("./data/SPDXJSONExample-v2.3.spdx.json", 20, 22), ("./data/SPDX.spdx", 11, 10)],
+)
+def test_generate_graph_from_file(
+    file_path: str, nodes_count: int, edges_count: int
+) -> None:
+    document = parse_file(os.path.join(os.path.dirname(__file__), file_path))
+    graph = generate_graph_from_file(document)
+
+    assert document.creation_info.spdx_id in graph.nodes()
+
+    assert len(graph.nodes()) == nodes_count
+    assert len(graph.edges()) == edges_count
