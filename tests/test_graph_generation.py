efa593aa9da6355486c274f451094b4dd6778e6d
Meret Behrens <meret.behrens@tngtech.com>
2023-03-29 14:11:17 +0200

    [issue_11] also test the existence of some relationship_node_keys
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/test_graph_generation.py
+++ tests/test_graph_generation.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 from pathlib import Path
+from typing import List
 
 import pytest
 from spdx.parser.parse_anything import parse_file
@@ -21,15 +22,27 @@ def test_generate_graph_from_spdx_lite() -> None:
 
     assert document.creation_info.spdx_id in graph.nodes()
     assert graph.number_of_nodes() == 4
-    assert len(graph.edges()) == 3
+    assert "DESCRIBES" in graph.nodes()
+    assert graph.number_of_edges() == 3
 
 
 @pytest.mark.parametrize(
-    "file_name, nodes_count, edges_count",
-    [("SPDXJSONExample-v2.3.spdx.json", 20, 22), ("SPDX.spdx", 11, 10)],
+    "file_name, nodes_count, edges_count, relationship_node_keys",
+    [
+        (
+            "SPDXJSONExample-v2.3.spdx.json",
+            20,
+            22,
+            ["SPDXRef-Package_DYNAMIC_LINK", "SPDXRef-JenaLib_CONTAINS"],
+        ),
+        ("SPDX.spdx", 11, 10, []),
+    ],
 )
 def test_generate_graph_from_spdx(
-    file_name: str, nodes_count: int, edges_count: int
+    file_name: str,
+    nodes_count: int,
+    edges_count: int,
+    relationship_node_keys: List[str],
 ) -> None:
     document = parse_file(str(Path(__file__).resolve().parent / "data" / file_name))
     validation_messages = validate_full_spdx_document(document)
@@ -37,6 +50,10 @@ def test_generate_graph_from_spdx(
 
     assert document.creation_info.spdx_id in graph.nodes()
 
-    assert len(graph.nodes()) == nodes_count
-    assert len(graph.edges()) == edges_count
+    assert graph.number_of_nodes() == nodes_count
+    assert graph.number_of_edges() == edges_count
+    assert "SPDXRef-DOCUMENT_DESCRIBES" in graph.nodes()
+    for relationship_node_key in relationship_node_keys:
+        assert relationship_node_key in graph.nodes()
+
     assert validation_messages == []
