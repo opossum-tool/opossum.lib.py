1f9b1db191589e05fc2111117754afb224eddd61
Meret Behrens <meret.behrens@tngtech.com>
2023-03-29 13:23:18 +0200

    [issue_11] squashed review commits
    - rename methods, variables
    - extract variable for "DESCRIBES"
    - add element information to node
    - use pathlib instead of os
    
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/opossum_lib/graph_generation.py
+++ src/opossum_lib/graph_generation.py
@@ -4,6 +4,7 @@
 from typing import Dict, List
 
 from networkx import DiGraph  # type: ignore
+from spdx.document_utils import get_element_from_spdx_id
 from spdx.model.document import Document
 from spdx.model.relationship import Relationship
 
@@ -14,47 +15,53 @@ def generate_graph_from_spdx_lite(document: Document) -> DiGraph:
     document_spdx_id = document.creation_info.spdx_id
     graph.add_node(document_spdx_id)
 
-    graph.add_node("DESCRIBES")
-    graph.add_edge(document_spdx_id, "DESCRIBES")
+    describes = "DESCRIBES"
+    graph.add_node(describes)
+    graph.add_edge(document_spdx_id, describes)
 
     package_spdx_ids = [package.spdx_id for package in document.packages]
-    graph.add_nodes_from(package_spdx_ids)
-    graph.add_edges_from(("DESCRIBES", spdx_id) for spdx_id in package_spdx_ids)
+    package_nodes = [
+        (spdx_id, {"element": get_element_from_spdx_id(document, spdx_id)})
+        for spdx_id in package_spdx_ids
+    ]
+    graph.add_nodes_from(package_nodes)
+    graph.add_edges_from((describes, spdx_id) for spdx_id in package_spdx_ids)
 
     return graph
 
 
-def generate_graph_from_file(document: Document) -> DiGraph:
+def generate_graph_from_spdx(document: Document) -> DiGraph:
     graph = DiGraph()
     graph.add_node(document.creation_info.spdx_id, element=document.creation_info)
 
     relationships_by_spdx_id: Dict[
         str, List[Relationship]
-    ] = get_relationships_by_spdx_id(document.relationships)
+    ] = _get_relationships_by_spdx_id(document.relationships)
 
-    for spdx_id in relationships_by_spdx_id.keys():
+    for spdx_id, relationships in relationships_by_spdx_id.items():
         if spdx_id not in graph.nodes():
-            graph.add_node(spdx_id)
-        for relationship in relationships_by_spdx_id[spdx_id]:
+            graph.add_node(spdx_id, element=get_element_from_spdx_id(document, spdx_id))
+        for relationship in relationships:
             relationship_node_key = (
-                str(relationship.spdx_element_id)
-                + "_"
-                + relationship.relationship_type.name
+                relationship.spdx_element_id + "_" + relationship.relationship_type.name
             )
             graph.add_node(relationship_node_key, comment=relationship.comment)
             graph.add_edge(relationship.spdx_element_id, relationship_node_key)
             # if the related spdx element is SpdxNone or SpdxNoAssertion we need a
             # type conversion
-            spdx_element_id = str(relationship.related_spdx_element_id)
+            related_spdx_element_id = str(relationship.related_spdx_element_id)
 
-            if spdx_element_id not in graph.nodes():
-                graph.add_node(spdx_element_id)
-            graph.add_edge(relationship_node_key, spdx_element_id)
+            if related_spdx_element_id not in graph.nodes():
+                graph.add_node(
+                    related_spdx_element_id,
+                    element=get_element_from_spdx_id(document, spdx_id),
+                )
+            graph.add_edge(relationship_node_key, related_spdx_element_id)
 
     return graph
 
 
-def get_relationships_by_spdx_id(
+def _get_relationships_by_spdx_id(
     relationships: List[Relationship],
 ) -> Dict[str, List[Relationship]]:
     relationships_by_spdx_id: Dict[str, List[Relationship]] = dict()
--- tests/test_graph_generation.py
+++ tests/test_graph_generation.py
@@ -1,22 +1,22 @@
 # SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-import os
+from pathlib import Path
 
 import pytest
 from spdx.parser.parse_anything import parse_file
+from spdx.validation.document_validator import validate_full_spdx_document
 
 from opossum_lib.graph_generation import (
-    generate_graph_from_file,
+    generate_graph_from_spdx,
     generate_graph_from_spdx_lite,
 )
 
 
 def test_generate_graph_from_spdx_lite() -> None:
     document = parse_file(
-        os.path.join(os.path.dirname(__file__), "./data/SPDXLite.spdx")
+        str(Path(__file__).resolve().parent / "data" / "SPDXLite.spdx")
     )
-
     graph = generate_graph_from_spdx_lite(document)
 
     assert document.creation_info.spdx_id in graph.nodes()
@@ -25,16 +25,18 @@ def test_generate_graph_from_spdx_lite() -> None:
 
 
 @pytest.mark.parametrize(
-    "file_path, nodes_count, edges_count",
-    [("./data/SPDXJSONExample-v2.3.spdx.json", 20, 22), ("./data/SPDX.spdx", 11, 10)],
+    "file_name, nodes_count, edges_count",
+    [("SPDXJSONExample-v2.3.spdx.json", 20, 22), ("SPDX.spdx", 11, 10)],
 )
-def test_generate_graph_from_file(
-    file_path: str, nodes_count: int, edges_count: int
+def test_generate_graph_from_spdx(
+    file_name: str, nodes_count: int, edges_count: int
 ) -> None:
-    document = parse_file(os.path.join(os.path.dirname(__file__), file_path))
-    graph = generate_graph_from_file(document)
+    document = parse_file(str(Path(__file__).resolve().parent / "data" / file_name))
+    validation_messages = validate_full_spdx_document(document)
+    graph = generate_graph_from_spdx(document)
 
     assert document.creation_info.spdx_id in graph.nodes()
 
     assert len(graph.nodes()) == nodes_count
     assert len(graph.edges()) == edges_count
+    assert validation_messages == []
