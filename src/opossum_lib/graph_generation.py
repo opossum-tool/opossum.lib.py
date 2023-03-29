39f8e2e9cc891c779f857a456cf10ee85545a2e5
Meret Behrens <meret.behrens@tngtech.com>
2023-03-29 14:27:08 +0200

    [issue_11] add nodes for all elements in the document
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/opossum_lib/graph_generation.py
+++ src/opossum_lib/graph_generation.py
@@ -4,7 +4,7 @@
 from typing import Dict, List
 
 from networkx import DiGraph  # type: ignore
-from spdx.document_utils import get_element_from_spdx_id
+from spdx.document_utils import get_contained_spdx_element_ids, get_element_from_spdx_id
 from spdx.model.document import Document
 from spdx.model.relationship import Relationship
 
@@ -33,6 +33,13 @@ def generate_graph_from_spdx_lite(document: Document) -> DiGraph:
 def generate_graph_from_spdx(document: Document) -> DiGraph:
     graph = DiGraph()
     graph.add_node(document.creation_info.spdx_id, element=document.creation_info)
+
+    contained_elements: List[str] = get_contained_spdx_element_ids(document)
+    contained_element_nodes = [
+        (spdx_id, {"element": get_element_from_spdx_id(document, spdx_id)})
+        for spdx_id in contained_elements
+    ]
+    graph.add_nodes_from(contained_element_nodes)
 
     relationships_by_spdx_id: Dict[
         str, List[Relationship]
