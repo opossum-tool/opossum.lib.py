9d1d5c3731c53f45ec62c6b00a557d91b266088b
Meret Behrens <meret.behrens@tngtech.com>
2023-03-30 15:26:07 +0200

    [issue_12] add logic to generate a tree from a given graph
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

---
+++ src/opossum_lib/tree_generation.py
@@ -0,0 +1,95 @@
+# SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+from typing import Dict, Optional, Tuple, Union
+
+from mypy.copytype import Any
+from networkx import (
+    DiGraph,
+    edge_bfs,
+    is_weakly_connected,
+    subgraph,
+    weakly_connected_components,
+)
+from spdx.model.document import CreationInfo
+from spdx.model.file import File
+from spdx.model.package import Package
+
+
+def generate_tree_from_graph(
+    graph: DiGraph,
+    source: Optional[str] = "SPDXRef-DOCUMENT",
+    tree: Optional[DiGraph] = None,
+) -> DiGraph:
+    if not graph.edges():
+        return graph
+
+    if is_weakly_connected(graph):
+        tree = tree or DiGraph()
+        edges_bfs = edge_bfs(graph, source)
+        visited_edges = []
+        for edge in edges_bfs:
+            _add_source_node(edge, graph, tree)
+            _add_target_node_and_edge(edge, graph, tree)
+            visited_edges += [edge]
+        # check if there is a directed path from source to each element node
+        # if not, we need to construct sub-graphs from the unreached edges
+        # and construct trees for all of these sub-graphs, the result
+        # will be an unconnected graph
+        unreached_edges = set(graph.edges).difference(set(visited_edges))
+        if unreached_edges:
+            induced_subgraph = graph.edge_subgraph(unreached_edges)
+            source = _get_node_without_incoming_edge(induced_subgraph)
+            tree_component = generate_tree_from_graph(induced_subgraph, source, tree)
+            tree.add_nodes_from(tree_component.nodes)
+            tree.add_edges_from(tree_component.edges)
+
+    else:  # get connected components
+        tree = DiGraph()
+        for connected_set in weakly_connected_components(
+            graph
+        ):  # returns only a set of nodes without edges
+            connected_subgraph = subgraph(graph, connected_set)
+            # if the documents node is not in the subgraph we choose
+            # any elements node without incoming edge
+            source = (
+                "SPDXRef-DOCUMENT"
+                if "SPDXRef-DOCUMENT" in connected_set
+                else _get_node_without_incoming_edge(connected_subgraph)
+            )
+            tree_component = generate_tree_from_graph(connected_subgraph, source)
+            tree.add_nodes_from(tree_component.nodes)
+            tree.add_edges_from(tree_component.edges)
+
+    return tree
+
+
+def _add_target_node_and_edge(
+    edge: Tuple[str, str], graph: DiGraph, tree: DiGraph
+) -> None:
+    if edge[1] not in tree:
+        target_node_data = graph.nodes[edge[1]]
+        tree.add_node(edge[1], **target_node_data)
+        tree.add_edge(*edge)
+    else:
+        # if the child node is already in the graph duplicate the node by adding the
+        # parent node as prefix
+        target_node_data = graph.nodes[edge[1]]
+        duplicated_node = edge[0] + "_" + edge[1]
+        tree.add_node(duplicated_node, **target_node_data)
+        tree.add_edge(edge[0], duplicated_node)
+
+
+def _add_source_node(edge: Tuple[str, str], graph: DiGraph, tree: DiGraph) -> None:
+    if edge[0] not in tree:
+        source_node_data: Dict[str, Union[CreationInfo, Package, File]] = graph.nodes[
+            edge[0]
+        ]
+        tree.add_node(edge[0], **source_node_data)
+
+
+def _get_node_without_incoming_edge(graph: DiGraph) -> Any:
+    for node, degree in graph.in_degree():
+        if degree == 0 and "element" in graph.nodes[node]:
+            return node
+    return ""
--- tests/test_graph_generation.py
+++ tests/test_graph_generation.py
@@ -1,22 +1,17 @@
 # SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-from datetime import datetime
 from pathlib import Path
 from typing import List
 from unittest import TestCase
 
 import pytest
-from spdx.model.actor import Actor, ActorType
-from spdx.model.checksum import Checksum, ChecksumAlgorithm
-from spdx.model.document import CreationInfo, Document
-from spdx.model.file import File
 from spdx.model.package import Package
-from spdx.model.relationship import Relationship, RelationshipType
 from spdx.parser.parse_anything import parse_file
 from spdx.validation.document_validator import validate_full_spdx_document
 
 from opossum_lib.graph_generation import generate_graph_from_spdx
+from tests.helper_methods import _create_minimal_document
 
 
 @pytest.mark.parametrize(
@@ -120,51 +115,3 @@ def test_complete_unconnected_graph() -> None:
             ("SPDXRef-Package-B_CONTAINS", "SPDXRef-File"),
         ],
     )
-
-
-def _create_minimal_document() -> Document:
-    # this is a helper method to create a minimal document that describes two
-    # packages which both contain the same file
-    creation_info = CreationInfo(
-        spdx_version="SPDX-2.3",
-        spdx_id="SPDXRef-DOCUMENT",
-        data_license="CC0-1.0",
-        name="SPDX Lite Document",
-        document_namespace="https://test.namespace.com",
-        creators=[Actor(ActorType.PERSON, "Meret Behrens")],
-        created=datetime(2023, 3, 14, 8, 49),
-    )
-    package_a = Package(
-        name="Example package A",
-        spdx_id="SPDXRef-Package-A",
-        download_location="https://download.com",
-    )
-    package_b = Package(
-        name="Example package B",
-        spdx_id="SPDXRef-Package-B",
-        download_location="https://download.com",
-    )
-    file = File(
-        name="Example file",
-        spdx_id="SPDXRef-File",
-        checksums=[Checksum(ChecksumAlgorithm.SHA1, "")],
-    )
-
-    relationships = [
-        Relationship(
-            "SPDXRef-DOCUMENT", RelationshipType.DESCRIBES, "SPDXRef-Package-A"
-        ),
-        Relationship(
-            "SPDXRef-DOCUMENT", RelationshipType.DESCRIBES, "SPDXRef-Package-B"
-        ),
-        Relationship("SPDXRef-Package-A", RelationshipType.CONTAINS, "SPDXRef-File"),
-        Relationship("SPDXRef-Package-B", RelationshipType.CONTAINS, "SPDXRef-File"),
-    ]
-    document = Document(
-        creation_info=creation_info,
-        packages=[package_a, package_b],
-        files=[file],
-        relationships=relationships,
-    )
-
-    return document
