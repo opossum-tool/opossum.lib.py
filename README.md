e15a294d990b1cb89b43efe8452a7ea1b8bb6a73
Meret Behrens <meret.behrens@tngtech.com>
2023-04-11 16:55:17 +0200

    [issue_2] introduce REUSE
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+<!--
+SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
+
+SPDX-License-Identifier: Apache-2.0
+-->
 # opossum.lib.py
--- scripts/linter_and_formatting.sh
+++ scripts/linter_and_formatting.sh
@@ -1,3 +1,6 @@
+# SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
+#
+# SPDX-License-Identifier: Apache-2.0
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
