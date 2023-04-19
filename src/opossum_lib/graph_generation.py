cb3d1d7e4538c7fcf861486ba44138f5b51d5183
Meret Behrens <meret.behrens@tngtech.com>
2023-04-19 13:54:41 +0200

    [issue_25] use graph generation from tools-python
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- src/opossum_lib/graph_generation.py
+++ src/opossum_lib/graph_generation.py
@@ -1,81 +1,16 @@
 # SPDX-FileCopyrightText: 2023 TNG Technology Consulting GmbH <https://www.tngtech.com>
 #
 # SPDX-License-Identifier: Apache-2.0
-from typing import Dict, List
-
 from networkx import DiGraph
-from spdx_tools.spdx.document_utils import (
-    get_contained_spdx_elements,
-    get_element_from_spdx_id,
-)
+from spdx_tools.spdx.graph_generation import generate_relationship_graph_from_spdx
 from spdx_tools.spdx.model.document import Document
-from spdx_tools.spdx.model.relationship import Relationship
 
 
 def generate_graph_from_spdx(document: Document) -> DiGraph:
-    graph = DiGraph()
-    graph.add_node(
-        document.creation_info.spdx_id,
-        element=document.creation_info,
-        label=document.creation_info.spdx_id,
-    )
-
-    contained_elements = get_contained_spdx_elements(document)
-    contained_element_nodes = [
-        (
-            spdx_id,
-            {
-                "element": element,
-                "label": element.name or spdx_id,
-            },
-        )
-        for spdx_id, element in contained_elements.items()
-    ]
-    graph.add_nodes_from(contained_element_nodes)
-
-    relationships_by_spdx_id: Dict[
-        str, List[Relationship]
-    ] = _get_relationships_by_spdx_id(document.relationships)
-
-    for spdx_id, relationships in relationships_by_spdx_id.items():
-        if spdx_id not in graph.nodes():
-            graph.add_node(
-                spdx_id,
-                element=get_element_from_spdx_id(document, spdx_id),
-                label=spdx_id,
-            )
-        for relationship in relationships:
-            relationship_node_key = (
-                relationship.spdx_element_id + "_" + relationship.relationship_type.name
-            )
-            graph.add_node(
-                relationship_node_key,
-                comment=relationship.comment,
-                label=relationship.relationship_type.name,
-            )
-            graph.add_edge(relationship.spdx_element_id, relationship_node_key)
-            # if the related spdx element is SpdxNone or SpdxNoAssertion we need a
-            # type conversion
-            related_spdx_element_id = str(relationship.related_spdx_element_id)
-
-            if related_spdx_element_id not in graph.nodes():
-                graph.add_node(
-                    related_spdx_element_id,
-                    element=get_element_from_spdx_id(document, related_spdx_element_id),
-                    label=related_spdx_element_id,
-                )
-            graph.add_edge(relationship_node_key, related_spdx_element_id)
-
+    graph = generate_relationship_graph_from_spdx(document)
+    for node in graph.nodes():
+        if "_" in node:
+            graph.add_node(node, label=node.split("_", 1)[-1])
+        else:
+            graph.add_node(node, label=node)
     return graph
-
-
-def _get_relationships_by_spdx_id(
-    relationships: List[Relationship],
-) -> Dict[str, List[Relationship]]:
-    relationships_by_spdx_id: Dict[str, List[Relationship]] = dict()
-    for relationship in relationships:
-        relationships_by_spdx_id.setdefault(relationship.spdx_element_id, []).append(
-            relationship
-        )
-
-    return relationships_by_spdx_id
