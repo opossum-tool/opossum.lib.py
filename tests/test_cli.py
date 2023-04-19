04e5f3598ce5bedd6cd0f6db8b728225b91101c3
Meret Behrens <meret.behrens@tngtech.com>
2023-04-19 13:31:44 +0200

    [issue_26] add validation to cli and more tests
    Signed-off-by: Meret Behrens <meret.behrens@tngtech.com>

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,13 +6,16 @@ from pathlib import Path
 from typing import Tuple
 
 import pytest
+from _pytest.logging import LogCaptureFixture
 from click.testing import CliRunner
+from spdx.writer.tagvalue.tagvalue_writer import write_document_to_file
 
 from opossum_lib.cli import spdx2opossum
+from tests.helper_methods import _create_minimal_document
 
 
 @pytest.mark.parametrize("options", [("--infile", "--outfile"), ("-i", "-o")])
-def test_cli(tmp_path: Path, options: Tuple[str, str]) -> None:
+def test_cli_with_system_exit_code_0(tmp_path: Path, options: Tuple[str, str]) -> None:
     runner = CliRunner()
 
     result = runner.invoke(
@@ -30,3 +33,33 @@ def test_cli(tmp_path: Path, options: Tuple[str, str]) -> None:
     with open(tmp_path / "output.json") as file:
         opossum_dict = json.load(file)
     assert "metadata" in opossum_dict
+
+
+def test_cli_with_system_exit_code_1() -> None:
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        with open("invalid_spdx.spdx", "w") as f:
+            f.write("SPDXID: SPDXRef-DOCUMENT")
+        result = runner.invoke(spdx2opossum, "-i invalid_spdx.spdx -o invalid")
+
+    assert result.exit_code == 1
+
+
+def test_cli_with_invalid_document(caplog: LogCaptureFixture) -> None:
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        create_invalid_spdx_document("invalid_spdx.spdx")
+        result = runner.invoke(spdx2opossum, "-i invalid_spdx.spdx -o invalid")
+
+    assert result.output == ""
+    assert caplog.messages == [
+        "The given SPDX document is not valid, this might cause issues with "
+        "the conversion."
+    ]
+
+
+def create_invalid_spdx_document(file_path: str) -> None:
+    document = _create_minimal_document()
+    document.creation_info.spdx_id = "DocumentID"
+
+    write_document_to_file(document, file_path, False)
