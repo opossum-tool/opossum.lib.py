34a735e27692abe9a47e1eac8bcb15fb58a1857d
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2025-01-20 08:26:27 +0100

    testing: introduce faker

--- tests/test_opossum/test_file_generation.py
+++ tests/test_opossum/test_file_generation.py
@@ -5,6 +5,8 @@
 from pathlib import Path
 from zipfile import ZipFile
 
+from faker import Faker
+
 import opossum_lib.opossum.output_model
 from opossum_lib.opossum.constants import INPUT_JSON_NAME, OUTPUT_JSON_NAME
 from opossum_lib.opossum.file_generation import write_opossum_information_to_file
@@ -12,33 +14,37 @@ from opossum_lib.opossum.opossum_file import Metadata, OpossumInformation
 from opossum_lib.opossum.opossum_file_content import OpossumFileContent
 from opossum_lib.opossum.output_model import OpossumOutputFile
 
-OPOSSUM_INFILE = OpossumInformation(
-    metadata=Metadata(
-        project_id="project-id",
-        file_creation_date="30-05-2023",
-        project_title="project-title",
-    ),
-    resources={},
-    external_attributions={},
-    resources_to_attributions={},
-)
 
-OPOSSUM_OUTFILE = OpossumOutputFile(
-    metadata=opossum_lib.opossum.output_model.Metadata(
-        project_id="project-id",
-        file_creation_date="30-05-2023",
-        input_file_md5_checksum="checksum",
-    ),
-    manual_attributions={},
-    resources_to_attributions={},
-    resolved_external_attributions=None,
-)
+def generate_opossum_outfile(faker: Faker) -> OpossumOutputFile:
+    return OpossumOutputFile(
+        metadata=opossum_lib.opossum.output_model.Metadata(
+            project_id="project-id-" + faker.word(),
+            file_creation_date=faker.date_time().isoformat(),
+            input_file_md5_checksum=faker.md5(),
+        ),
+        manual_attributions={},
+        resources_to_attributions={},
+        resolved_external_attributions=None,
+    )
+
+
+def generate_opossum_information(faker: Faker)-> OpossumInformation:
+    return OpossumInformation(
+        metadata=Metadata(
+            project_id="project-id-" + faker.word(),
+            file_creation_date=faker.date_time().isoformat(),
+            project_title="project-id-" + faker.word(),
+        ),
+        resources={},
+        external_attributions={},
+        resources_to_attributions={},
+    )
 
 
 def test_only_input_information_available_writes_only_input_information(
-    tmp_path: Path,
+    tmp_path: Path, faker: Faker
 ) -> None:
-    opossum_file_content = OpossumFileContent(OPOSSUM_INFILE)
+    opossum_file_content = OpossumFileContent(generate_opossum_information(faker))
     output_path = tmp_path / "output.opossum"
 
     write_opossum_information_to_file(opossum_file_content, output_path)
@@ -47,10 +53,11 @@ def test_only_input_information_available_writes_only_input_information(
         assert zip_file.namelist() == [INPUT_JSON_NAME]
 
 
-def test_input_and_output_information_available_writes_both(tmp_path: Path) -> None:
+def test_input_and_output_information_available_writes_both(tmp_path: Path, faker: Faker) -> None:
     opossum_file_content = OpossumFileContent(
-        input_file=OPOSSUM_INFILE, output_file=OPOSSUM_OUTFILE
+        input_file=generate_opossum_information(faker), output_file=generate_opossum_outfile(faker)
     )
+    print(opossum_file_content)
     output_path = tmp_path / "output.opossum"
 
     write_opossum_information_to_file(opossum_file_content, output_path)
