9152f8c9c0644468a7c3ef4d6983cca3c0eab6f3
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2025-01-24 09:17:27 +0100

    refactor: Create converter class
    Goal: encapsulating the conversion methods

--- src/opossum_lib/opossum/read_opossum_file.py
+++ src/opossum_lib/opossum/read_opossum_file.py
@@ -4,12 +4,12 @@
 
 from opossum_lib.opossum.opossum_file_content import OpossumFileContent
 from opossum_lib.opossum.opossum_file_to_opossum_converter import (
-    convert_to_opossum,
+    OpossumFileToOpossumConverter,
 )
 
 
 def read_opossum_file(filename: str) -> OpossumFileContent:
     opossum_input_file = OpossumFileContent.from_file(file_name=filename)
-    opossum = convert_to_opossum(opossum_input_file)
+    opossum = OpossumFileToOpossumConverter.convert_to_opossum(opossum_input_file)
 
     return opossum.to_opossum_file_format()
