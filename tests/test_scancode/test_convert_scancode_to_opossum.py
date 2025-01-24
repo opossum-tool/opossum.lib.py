e70251687a21e80948fa50caef7677ce341bddb7
Adrian Braemer <adrian.braemer@tngtech.com>
2025-01-24 07:50:13 +0100

    tests: change tests to use the new generator

--- tests/test_scancode/test_convert_scancode_to_opossum.py
+++ tests/test_scancode/test_convert_scancode_to_opossum.py
@@ -2,8 +2,6 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-import json
-from pathlib import Path
 
 import pytest
 from _pytest.logging import LogCaptureFixture
@@ -11,20 +9,19 @@ from _pytest.logging import LogCaptureFixture
 from opossum_lib.scancode.convert_scancode_to_opossum import (
     extract_scancode_header,
 )
-from opossum_lib.scancode.model import ScanCodeData
-
-TEST_SCANCODE_FILE = str(Path(__file__).parent.parent / "data/scancode_input.json")
+from tests.test_setup.scancode_faker_setup import ScanCodeFaker
 
 
-def test_extract_scancode_header() -> None:
-    scancode_data = _create_valid_scancode_data()
+def test_extract_scancode_header(scancode_faker: ScanCodeFaker) -> None:
+    scancode_data = scancode_faker.scancode_data()
     extracted_header = extract_scancode_header(scancode_data, "test/path/scancode.json")
     assert extracted_header == scancode_data.headers[0]
 
 
-def test_extract_scancode_header_missing_header(caplog: LogCaptureFixture) -> None:
-    scancode_data = _create_valid_scancode_data()
-    scancode_data.headers = []
+def test_extract_scancode_header_missing_header(
+    caplog: LogCaptureFixture, scancode_faker: ScanCodeFaker
+) -> None:
+    scancode_data = scancode_faker.scancode_data(headers=[])
 
     with pytest.raises(SystemExit):
         extract_scancode_header(scancode_data, "test/path/scancode.json")
@@ -32,17 +29,14 @@ def test_extract_scancode_header_missing_header(caplog: LogCaptureFixture) -> No
     assert "header" in caplog.messages[0].lower()
 
 
-def test_extract_scancode_header_multiple_headers(caplog: LogCaptureFixture) -> None:
-    scancode_data = _create_valid_scancode_data()
-    scancode_data.headers.append(scancode_data.headers[0])
+def test_extract_scancode_header_multiple_headers(
+    caplog: LogCaptureFixture, scancode_faker: ScanCodeFaker
+) -> None:
+    header1 = scancode_faker.header()
+    header2 = scancode_faker.header()
+    scancode_data = scancode_faker.scancode_data(headers=[header1, header2])
 
     with pytest.raises(SystemExit):
         extract_scancode_header(scancode_data, "test/path/scancode.json")
 
     assert "header" in caplog.messages[0].lower()
-
-
-def _create_valid_scancode_data() -> ScanCodeData:
-    with open(TEST_SCANCODE_FILE) as inp:
-        json_data = json.load(inp)
-    return ScanCodeData.model_validate(json_data)
--- tests/test_scancode/test_resource_tree.py
+++ tests/test_scancode/test_resource_tree.py
@@ -2,83 +2,59 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-from copy import deepcopy
 
 from opossum_lib.opossum_model import OpossumPackage, SourceInfo
 from opossum_lib.scancode.constants import SCANCODE_SOURCE_NAME
-from opossum_lib.scancode.model import (
-    Copyright,
-    FileBasedLicenseDetection,
-    FileType,
-    Match,
-)
 from opossum_lib.scancode.resource_tree import (
     get_attribution_info,
 )
-from tests.test_scancode.model_helpers import _create_file
+from tests.test_setup.scancode_faker_setup import ScanCodeFaker
 
 
-def test_get_attribution_info_directory() -> None:
-    folder = _create_file("A", FileType.DIRECTORY)
+def test_get_attribution_info_directory(scancode_faker: ScanCodeFaker) -> None:
+    folder = scancode_faker.single_folder(path="some/single/folder")
     assert get_attribution_info(folder) == []
 
 
-def test_get_attribution_info_file_missing() -> None:
-    file = _create_file("A", FileType.FILE)
+def test_get_attribution_info_from_file_without_detections(
+    scancode_faker: ScanCodeFaker,
+) -> None:
+    file = scancode_faker.single_file(path="some/single/file", license_detections=[])
     assert get_attribution_info(file) == []
 
 
-def test_get_attribution_info_file_multiple() -> None:
-    match1 = Match(
-        license_expression="apache-2.0",
+def test_get_attribution_info_file_multiple(scancode_faker: ScanCodeFaker) -> None:
+    match1 = scancode_faker.match(
         license_expression_spdx="Apache-2.0",
         from_file="A",
-        start_line=1,
-        end_line=2,
-        matcher="matcher",
         score=75,
-        matched_length=1,
-        match_coverage=0.5,
         rule_relevance=50,
-        rule_identifier="myrule",
-        rule_url="",
     )
-    match2 = Match(
-        license_expression="apache-2.0",
+    match2 = scancode_faker.match(
         license_expression_spdx="Apache-2.0",
         from_file="A",
-        start_line=2,
-        end_line=3,
-        matcher="matcher",
         score=95,
-        matched_length=1,
-        match_coverage=0.5,
         rule_relevance=50,
-        rule_identifier="hyrule",
-        rule_url="",
     )
-    match3 = deepcopy(match1)
-    match3.score = 50
-    match3.license_expression = "mit"
-    match3.license_expression_spdx = "MIT"
-    license1 = FileBasedLicenseDetection(
-        license_expression="apache-2.0",
+    match3 = scancode_faker.match(
+        license_expression_spdx="MIT",
+        from_file="A",
+        score=50,
+        rule_relevance=50,
+    )
+    license1 = scancode_faker.license_detection(
         license_expression_spdx="Apache-2.0",
-        identifier="identifier1",
         matches=[match1, match2],
     )
-    license2 = FileBasedLicenseDetection(
-        license_expression="mit",
+    license2 = scancode_faker.license_detection(
         license_expression_spdx="MIT",
-        identifier="identifier2",
         matches=[match3],
     )
-    copyright1 = Copyright(copyright="Me", start_line=1, end_line=2)
-    copyright2 = Copyright(copyright="Myself", start_line=1, end_line=2)
-    copyright3 = Copyright(copyright="I", start_line=1, end_line=2)
-    file = _create_file(
-        "A",
-        FileType.FILE,
+    copyright1 = scancode_faker.copyright(copyright="Me")
+    copyright2 = scancode_faker.copyright(copyright="Myself")
+    copyright3 = scancode_faker.copyright(copyright="I")
+    file = scancode_faker.single_file(
+        path="A",
         license_detections=[license1, license2],
         copyrights=[copyright1, copyright2, copyright3],
     )
