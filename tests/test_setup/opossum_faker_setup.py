4111fcbf251dc10f97d8f53c74173ac9f71bfb9b
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2025-01-27 10:51:28 +0100

    refactor: shorten faker setup

--- tests/test_setup/opossum_faker_setup.py
+++ tests/test_setup/opossum_faker_setup.py
@@ -7,17 +7,6 @@ from typing import Any, cast
 
 from faker import Faker, Generator
 
-from opossum_lib.opossum.output_model import OpossumOutputFile
-from opossum_lib.opossum_model import (
-    BaseUrlsForSources,
-    ExternalAttributionSource,
-    FrequentLicense,
-    Metadata,
-    Opossum,
-    OpossumPackage,
-    Resource,
-    ScanResults,
-)
 from tests.opossum_model_generators.opossum_provider import OpossumProvider
 from tests.opossum_model_generators.scan_results_provider import ScanResultsProvider
 
@@ -40,40 +29,8 @@ class OpossumFaker(Faker):
         )
         self.opossum_provider = OpossumProvider(self)
         self.scan_results_provider = ScanResultsProvider(self)
-
-    def opossum(
-        self,
-        scan_results: ScanResults | None = None,
-        review_results: OpossumOutputFile | None = None,
-    ) -> Opossum:
-        return self.opossum_provider.opossum(
-            scan_results=scan_results, review_results=review_results
-        )
-
-    def scan_results(
-        self,
-        metadata: Metadata | None = None,
-        resources: list[Resource] | None = None,
-        attribution_breakpoints: list[str] | None = None,
-        external_attribution_sources: dict[str, ExternalAttributionSource]
-        | None = None,
-        frequent_licenses: list[FrequentLicense] | None = None,
-        files_with_children: list[str] | None = None,
-        base_urls_for_sources: BaseUrlsForSources | None = None,
-        generate_attribution_to_id: bool = False,
-        unassigned_attributions: list[OpossumPackage] | None = None,
-    ) -> ScanResults:
-        return self.scan_results_provider.scan_results(
-            metadata=metadata,
-            resources=resources,
-            attribution_breakpoints=attribution_breakpoints,
-            external_attribution_sources=external_attribution_sources,
-            frequent_licenses=frequent_licenses,
-            files_with_children=files_with_children,
-            base_urls_for_sources=base_urls_for_sources,
-            generate_attribution_to_id=generate_attribution_to_id,
-            unassigned_attributions=unassigned_attributions,
-        )
+        self.opossum = self.opossum_provider.opossum
+        self.scan_results = self.scan_results_provider.scan_results
 
 
 def setup_opossum_faker(faker: Faker) -> OpossumFaker:
