ee39fb6850d7a74520589ad994b21ea5c8fc20fd
Dominikus Hellgartner <dominikus.hellgartner@tngtech.com>
2025-01-27 08:50:25 +0100

    refactor: encapsulate in classes

--- src/opossum_lib/opossum/file_generation.py
+++ src/opossum_lib/opossum/file_generation.py
@@ -17,34 +17,41 @@ from opossum_lib.opossum.opossum_file import (
 from opossum_lib.opossum.opossum_file_content import OpossumFileContent
 
 
-def write_opossum_information_to_file(
-    opossum_file_content: OpossumFileContent, file_path: Path
-) -> None:
-    with ZipFile(
-        file_path, "w", compression=ZIP_DEFLATED, compresslevel=COMPRESSION_LEVEL
-    ) as zip_file:
-        write_input_json(opossum_file_content, zip_file)
-        write_output_json_if_existing(opossum_file_content, zip_file)
+class OpossumFileWriter:
+    @staticmethod
+    def write_opossum_information_to_file(
+        opossum_file_content: OpossumFileContent, file_path: Path
+    ) -> None:
+        with ZipFile(
+            file_path, "w", compression=ZIP_DEFLATED, compresslevel=COMPRESSION_LEVEL
+        ) as zip_file:
+            OpossumFileWriter._write_input_json(opossum_file_content, zip_file)
+            OpossumFileWriter._write_output_json_if_existing(
+                opossum_file_content, zip_file
+            )
 
+    @staticmethod
+    def _write_output_json_if_existing(
+        opossum_file_content: OpossumFileContent, zip_file: ZipFile
+    ) -> None:
+        if opossum_file_content.output_file:
+            zip_file.writestr(
+                OUTPUT_JSON_NAME,
+                opossum_file_content.output_file.model_dump_json(
+                    exclude_none=True, indent=4, by_alias=True
+                ),
+            )
 
-def write_output_json_if_existing(
-    opossum_file_content: OpossumFileContent, zip_file: ZipFile
-) -> None:
-    if opossum_file_content.output_file:
+    @staticmethod
+    def _write_input_json(
+        opossum_file_content: OpossumFileContent, zip_file: ZipFile
+    ) -> None:
         zip_file.writestr(
-            OUTPUT_JSON_NAME,
-            opossum_file_content.output_file.model_dump_json(
-                exclude_none=True, indent=4, by_alias=True
+            INPUT_JSON_NAME,
+            TypeAdapter(OpossumInformation).dump_json(
+                opossum_file_content.input_file,
+                indent=4,
+                exclude_none=True,
+                by_alias=True,
             ),
         )
-
-
-def write_input_json(
-    opossum_file_content: OpossumFileContent, zip_file: ZipFile
-) -> None:
-    zip_file.writestr(
-        INPUT_JSON_NAME,
-        TypeAdapter(OpossumInformation).dump_json(
-            opossum_file_content.input_file, indent=4, exclude_none=True, by_alias=True
-        ),
-    )
